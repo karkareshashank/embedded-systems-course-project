8a8f439069f4ac048c4f0625a8dbf31c59e5bee1
shashank <shashank@shashank-M14xR1.(none)>
2013-09-06 14:27:07 -0700

    gmem_tester is in the mid .. Mydriver needs the wrap around code.

---
+++ Project1/gmem_tester.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <error.h>
+#include <errno.h>
+
+
+/* Function to print the usage message */
+void gmem_tester_usage()
+{
+	printf("[usage]: gmem_tester show             : to view th buffer content \n");
+        printf("      or gmem_tester write <string>   : to write the string to the buffer \n");
+}
+
+
+
+// Main function
+int main(int argc,char** argv)
+{
+
+	int fd;				// file descriptor
+	char argument[10];
+	char *in_string;
+	int i;
+
+	// Check for the argument 
+	if(argc == 1 )	
+	{
+		printf("Error: Command need atleast 1 argument \n");
+		gmem_tester_usage();
+		exit(1);
+	}
+
+	//Opening the device file to read or write
+	fd = open("/dev/gmem",O_RDWR);
+	if(fd == -1)	
+	{
+		printf("Error : %s \n",strerror(errno));
+		exit(1);
+	}
+
+	// Allocating space for in_string variable
+	in_string = (char*)malloc(sizeof(char)*1000);
+	in_string[0] = '\0';
+
+	// Copying the argument in another variable
+	strcpy(argument,argv[1]);
+
+
+	if(strcmp(argument,"show") == 0)			// If the argument is show , read from the buffer and print //
+	{
+		if(argc > 2)
+		{
+			printf("Error: No argument needed with show\n");
+			gmem_tester_usage();
+			goto exit;
+		}
+		printf("show \n");
+
+	}
+	else if(strcmp(argument,"write") == 0)			// If the argument is write , write to the buffer with the given string //
+	{
+		if(argc <=2)
+		{
+			printf("Error: No string provided\n");
+			gmem_tester_usage();
+			goto exit;
+		}
+		printf("write\n");
+		strcpy(in_string,argv[2]);
+		if(argc > 3)
+		{
+			for(i = 3; i < argc;i++)
+			{
+				strcat(in_string," ");
+				strcat(in_string,argv[i]);
+			}
+
+		}
+
+		printf("%s\n",in_string);
+
+	}
+	else							// Else its a wrong argument to the command //
+	{
+		printf("Error: Invalid argument to the command\n");
+		gmem_tester_usage();
+		goto exit;
+	}
+	
+
+   exit:
+	free(in_string);
+	close(fd);
+	return 0;
+}

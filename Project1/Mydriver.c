b8db0b638d65021bb9d6f340714668624f0a73a6
shashank <shashank@shashank-M14xR1.(none)>
2013-09-05 23:04:49 -0700

    Currently working on write module of the driver

--- Project1/Mydriver.c
+++ Project1/Mydriver.c
@@ -11,13 +11,18 @@
 #include <linux/spinlock.h>
 
 
-#define DEVICE_NAME                 "My_device1"
+#define DEVICE_NAME                 	"gmem"
+#define MEMORY_BUFFER_SIZE		256
+rwlock_t  mr_rwlock;			/* Lock for simultaneous read/write purpose 
+					 * Concurrency issue			    
+					 */
+
 
 /* per device structure */
 struct My_dev {
 	struct cdev cdev;               /* The cdev structure */
 	char name[20];                  /* Name of device*/
-	char in_string[80];			/* buffer for the input string */
+	char *mem_buffer;		/* buffer for the input string */
 } *my_devp;
 
 static dev_t my_dev_number;      /* Allotted device number */
@@ -62,12 +67,22 @@ int My_driver_release(struct inode *inode, struct file *file)
 ssize_t My_driver_write(struct file *file, const char *buf,
            size_t count, loff_t *ppos)
 {
-	int res;
+	size_t leftover;
+	int res1;
+	int res2;
 	struct My_dev *my_devp = file->private_data;
 	
-	res = copy_from_user((void *)&my_devp->in_string, (void __user *)buf, count);	
+	if(count < MEMORY_BUFFER_SIZE)			// If string is smaller than buffer size //
+		res1 = copy_from_user((void *)&my_devp->mem_buffer, (void __user *)buf, count);	
+	else						// If string is bigger than buffer size wrap around //
+	{
+		leftover = MEMORY_BUFFER_SIZE - count;
+		res1 = copy_from_user((void *)&my_devp->mem_buffer, (void __user *)buf, MEMORY_BUFFER_SIZE);
+		res2 = copy_from_user((void *)&my_devp->mem_buffer, (void __user *)
 
-	printk("\n%s \n", my_devp->in_string);
+	}
+
+	printk("\n%s \n", my_devp->mem_buffer);
 
 	return res;
 }
@@ -88,6 +103,9 @@ int __init My_driver_init(void)
 {
 	int ret;
 
+	/* initializing the read/write lock */
+	rwlock_init(&mr_rwlock);
+
 	/* Request dynamic allocation of a device major number */
 	if (alloc_chrdev_region(&my_dev_number, 0, 1, DEVICE_NAME) < 0) {
 			printk(KERN_DEBUG "Can't register device\n"); return -1;
@@ -102,6 +120,13 @@ int __init My_driver_init(void)
 		
 	if (!my_devp) {
 		printk("Bad Kmalloc\n"); return -ENOMEM;
+	}
+
+	/* Allocate memory for the string */
+	my_devp->mem_buffer = kmalloc(sizeof(char) * MEMORY_BUFFER_SIZE, GFP_KERNEL);
+
+	if(!(my_devp->mem_buffer)){
+		printk("Bad kamlloc\n"); return -ENOMEM;
 	}
 
 	/* Request I/O region */
@@ -137,6 +162,7 @@ void __exit My_driver_exit(void)
 	/* Destroy device */
 	device_destroy (my_dev_class, MKDEV(MAJOR(my_dev_number), 0));
 	cdev_del(&my_devp->cdev);
+	kfree(my_devp->mem_buffer);
 	kfree(my_devp);
 	
 	/* Destroy driver_class */
